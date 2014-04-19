043dcc267608096d29257f7fccab965a720c3d8e
Satoshi Nakagawa <nakasen_20th@mac.com>
2014-04-19 16:50:23 +0900

    Initial commit.

---
+++ TwitterClient01/TimeLineTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  TimeLineTableViewController.h
+//  TwitterClient01
+//
+//  Created by Satoshi Nakagawa on 2014/04/12.
+//  Copyright (c) 2014年 nakasen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+#import "TimeLineCell.h"
+#import "DetailViewController.h"
+
+@interface TimeLineTableViewController : UITableViewController
+
+@property (nonatomic) NSString *identifier;
+
+@end
---
+++ TwitterClient01/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  TwitterClient01
+//
+//  Created by Satoshi Nakagawa on 2014/04/12.
+//  Copyright (c) 2014年 nakasen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+#import "MyUIApplication.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, NSStringFromClass([MyUIApplication class]), NSStringFromClass([AppDelegate class]));
+    }
+}
